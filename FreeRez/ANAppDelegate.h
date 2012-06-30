129042a7e58a3a9f03b7b68edb6b2ce0656fdcc7
Alex Nichol <alexnichol@comcast.net>
2012-06-30 11:55:44 -0400

    Initial Commit

---
+++ FreeRez/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  FreeRez
+//
+//  Created by Alex Nichol on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FreeRez/ANAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ANAppDelegate.m
+//  FreeRez
+//
+//  Created by Alex Nichol on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ANAppDelegate.h"
+
+@implementation ANAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
